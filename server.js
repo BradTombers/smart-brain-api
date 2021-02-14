b8b9df63508fdb5901fcaffd19e78731592087d0
Brad Tombers <Brad.Tombers@gmail.com>
2021-02-14 07:34:52 -0600

    updating database

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'process.env.DATABASE_URL',
-     ssl: {
+    connectionString : 'process.env.DATABASE_URL',
+ 	ssl: {
    		rejectUnauthorized: false
-  }
+  	}
   }
 });
 
